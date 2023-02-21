a50b751f6d39bac9e1a8f4f4b1b4442f56a9ba38
donvelas <donv1337@gmail.com>
2023-02-20 19:04:52 -0800

    "open file button now opens to file explorer"

--- rfid.py
+++ rfid.py
@@ -1,10 +1,9 @@
-#dependancies 
+#dependencies to pip install 
 # pandas, numpy, xlrd
 
 from tkinter import *
 import pandas as pd
 from tkinter import ttk, filedialog
-
 
 root = Tk()
 root.title('metrics')
@@ -18,15 +17,17 @@ my_frame.pack(pady=20)
 # create tree view
 my_tree = ttk.Treeview()
 
-# file open function
+# file open function, this will do all the work
 def file_open():
-    pass
+    file_name = filedialog.askopenfile(
+        initialdir=""
+    )
 
-# add a menu
+# add a menu to the window
 my_menu = Menu(root)
 root.config(menu=my_menu)
 
-#add menu botton
+# add menu button to open spreadsheet
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="Speadsheets", menu=file_menu)
 file_menu.add_command(label="Open", command=file_open)
