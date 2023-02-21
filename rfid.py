ff09c187010f187d15f66dba0e49c75d0fbe1990
donvelas <donv1337@gmail.com>
2023-02-20 19:23:23 -0800

    "Added error messages"

--- rfid.py
+++ rfid.py
@@ -19,9 +19,30 @@ my_tree = ttk.Treeview()
 
 # file open function, this will do all the work
 def file_open():
-    file_name = filedialog.askopenfile(
-        initialdir=""
+    filename = filedialog.askopenfile(
+        initialdir='',
+        title="Open a file",
+        filetypes=(("xlsx files", ".xlsx"), ("All files", "*.*"))
     )
+    
+    if filename:
+        try:
+            filename = r"{}".format(filename)
+            df = pd.read_excel(filename)
+        except ValueError:
+            my_label.config(text="File couldn't be opened, try again")
+        except FileNotFoundError:
+            my_label.config(text="File not found")
+    
+    # clear old tree view        
+    clear_tree()
+    
+# clear tree function 
+def clear_tree():
+    my_tree.delete(*my_tree.get_children())
+            
+        
+        
 
 # add a menu to the window
 my_menu = Menu(root)
@@ -31,5 +52,9 @@ root.config(menu=my_menu)
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="Speadsheets", menu=file_menu)
 file_menu.add_command(label="Open", command=file_open)
+
+# Labels for error codes
+my_label = Label(root, text='')
+my_label.pack(pady=20)
 
 root.mainloop()
